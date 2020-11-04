0a627a405a283b54f0381db6ab4309f71ad4a197
alec-jasik <alec_jasik@yahoo.com>
2020-11-04 11:29:18 -0600

    submission for deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,29 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+ 
+  componentDidMount() {
+    fetch("http://localhost:6001/transactions")
+    .then((res) => res.json())
+    .then((data)=> {
+      this.setState({
+        transactions: data,
+      });
+    });
+  };
+
+  displaytransaction = (transaction) => {
+    console.log(transaction)
+    this.setState({
+      displaytransaction: this.state.transactions, transaction
+    })
+  }
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.props.transactions} />
       </div>
     );
   }
