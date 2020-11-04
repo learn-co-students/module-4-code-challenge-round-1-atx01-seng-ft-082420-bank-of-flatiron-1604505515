a50e6cae6eb84d5fd9a6fe0dbbab655c57210a5a
siegelverse <msiegel37@gmail.com>
2020-11-04 11:41:03 -0600

    marcus-siegel

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,63 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+    constructor() {
+      super()
+    
+      this.state = {
+         allTransactions: [],
+         search: ""
+      }
+    }
+    
+    componentDidMount = () => {
+      fetch("http://localhost:6001/transactions")
+      .then(res => res.json())
+      .then(allTransactions => {
+        this.setState({
+          allTransactions
+        })
+      })
+    }
+
+    transactionSearch = (e) => {
+      console.log(e)
+      let search = e
+      this.setState({
+        search
+      })
+    }
+
+    addNewTransaction = (newTransaction) => {
+      fetch("http://localhost:6001/transactions", {
+        method: "POST", 
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          date: newTransaction.date,
+          description: newTransaction.description, 
+          category: newTransaction.category,
+          amount: newTransaction.price
+        })
+      })
+      .then(res => res.json())
+      .then(newTrans => 
+        this.setState({
+          // allTransactions: [...this.state.allTransactions, newTrans]
+        })
+      )
+    }
+
   render() {
+    let filteredTransactions = this.state.allTransactions.filter(transaction => 
+      transaction.description.toLowerCase().includes(this.state.search.toLowerCase()))
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search transactionSearch={this.transactionSearch}/>
+        <AddTransactionForm addNewTransaction={this.addNewTransaction} />
+        <TransactionsList allTransactions={filteredTransactions}/>
       </div>
     );
   }
