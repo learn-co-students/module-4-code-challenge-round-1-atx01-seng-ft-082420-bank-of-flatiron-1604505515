46d77b6d3186f957a42f2a4299f0b2ed7b0c3501
hproctor021 <hproctor021@gmail.com>
2020-11-04 11:32:54 -0600

    submission for deadline

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: "",
+    description: "",
+    category: "",
+    amount: ""
+  }
+
+  handleChange = (e) => {
+    let {value, name} = e.target
+    // console.log(value, name)
+    this.setState({
+      [name] :value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addTransaction(this.state)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={this.handleChange}/>
+            <input type="text" name="description" placeholder="Description" onChange={this.handleChange}/>
+            <input type="text" name="category" placeholder="Category" onChange={this.handleChange}/>
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              onChange={this.handleChange}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" type="submit" >
             Add Transaction
           </button>
         </form>
