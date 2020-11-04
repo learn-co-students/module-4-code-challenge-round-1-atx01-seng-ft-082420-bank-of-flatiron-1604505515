a1235eb53b1ba8735797b628150e36042f1124fb
github username <github email address>
2020-11-04 11:23:20 -0600

    deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,15 +1,89 @@
-import React, { Component } from "react";
-import TransactionsList from "./TransactionsList";
-import Search from "./Search";
-import AddTransactionForm from "./AddTransactionForm";
+import React, { Component } from 'react';
+import TransactionsList from './TransactionsList';
+import Search from './Search';
+import AddTransactionForm from './AddTransactionForm';
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    searchTerm: '',
+    checked: ''
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:6001/transactions')
+      .then(res => res.json())
+      .then(transactions => {
+        // console.log(transactions);
+        this.setState({ transactions });
+      });
+  }
+
+  addNewTransaction = newTransaction => {
+    // console.log('InaddNEw', newTransaction);
+    fetch('http://localhost:6001/transactions', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(newTransaction)
+    })
+      .then(res => res.json())
+      .then(transactionObj => {
+        // console.log('Res from server', transactionObj);
+        this.setState({
+          transactions: [...this.state.transactions, transactionObj]
+        });
+      });
+  };
+
+  handleSearch = term => {
+    let searchTerm = term.toLowerCase();
+    this.setState({ searchTerm });
+  };
+
+  handleCheck = e => {
+    let check = e.target.value;
+    switch (check) {
+      case 'Category':
+        return this.setState({ checked: 'Category' });
+
+      case 'Description':
+        return this.setState({ checked: 'Description' });
+      default:
+        return this.state.transactions;
+    }
+  };
+
+  sortTransactions = () => {
+    let sort = this.state.checked;
+    switch (sort) {
+      case 'Category':
+        return this.state.transactions.sort((a, b) => {
+          return a.category.localeCompare(b.category);
+        });
+      case 'Description':
+        return this.state.transactions.sort((a, b) => {
+          return a.description.localeCompare(b.description);
+        });
+      default:
+        return this.state.transactions;
+    }
+  };
   render() {
+    // let sorted = this.state.transactions.sortTransactions().filter(transaction => )
+    let searched = this.state.transactions.filter(transaction =>
+      transaction.description.toLowerCase().includes(this.state.searchTerm)
+    );
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search
+          handleSearch={this.handleSearch}
+          handleCheck={this.handleCheck}
+        />
+        <AddTransactionForm addNewTransaction={this.addNewTransaction} />
+        <TransactionsList transactions={searched} />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,53 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class AddTransactionForm extends Component {
+  state = {
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // console.log(this.state);
+    this.props.addNewTransaction(this.state);
+  };
+
+  handleChange = e => {
+    let { value, name } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
-      <div className="ui segment">
-        <form className="ui form">
-          <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+      <div className='ui segment'>
+        <form onSubmit={this.handleSubmit} className='ui form'>
+          <div className='inline fields'>
+            <input type='date' name='date' onChange={this.handleChange} />
             <input
-              type="number"
-              name="amount"
-              placeholder="Amount"
-              step="0.01"
+              type='text'
+              name='description'
+              placeholder='Description'
+              onChange={this.handleChange}
+            />
+            <input
+              type='text'
+              name='category'
+              placeholder='Category'
+              onChange={this.handleChange}
+            />
+            <input
+              type='number'
+              name='amount'
+              placeholder='Amount'
+              step='0.01'
+              onChange={this.handleChange}
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className='ui button' type='submit'>
             Add Transaction
           </button>
         </form>
